653d6bb8225dd5fb7f0a4b27e4244b1c32fdfa5d
radek.scheller <radoslaw.scheller@gmail.com>
2022-12-31 11:35:30 +0100

    dodaje zadanie palindromy

--- palindroms.py
+++ palindroms.py
@@ -1,1 +1,14 @@
 print("Zadanie z palindromami")
+
+def isPalindrome(word):
+    return word == word[::-1]
+ 
+word = input("podaj slowo:")
+reverse = isPalindrome(word)
+ 
+if reverse:
+    print("Tak to palindrom")
+else:
+    print("Nie, to nie palindrom")
+
+isPalindrome(word)

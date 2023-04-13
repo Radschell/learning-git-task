83c331ca6ac135d6cfc8c456ccda8104b5e0ab99
radek.scheller <radoslaw.scheller@gmail.com>
2023-04-13 21:33:43 +0200

    roziwazanie zadan cz.1

--- music_chart.py
+++ music_chart.py
@@ -18,3 +18,43 @@ for index, row in album_table.iterrows():
 
 # display the DataFrame
 print(official_chart)
+
+
+# rename the column names for pl
+official_chart = official_chart.rename(columns={'title': 'TYTUŁ', 'artist': 'ARTYSTA', 'year': 'ROK', 'HIGH POSN': 'MAX POZ'})
+
+# script to count single artists
+
+artist_counts = official_chart['ARTYSTA'].value_counts()
+unique_artists = artist_counts[artist_counts == 1].index.tolist()
+
+print(f'The following artists appear only once in the table: {unique_artists}')
+
+#script to find most common artist
+most_common_artist = official_chart['ARTYSTA'].mode()[0]'
+
+#change column headers to have capital letter
+def rename_columns(dataframe):
+    # create a dictionary to map the old column names to the new column names
+    column_mapping = {col: col.capitalize() for col in dataframe.columns}
+    # rename the columns in the DataFrame using the mapping dictionary
+    dataframe = dataframe.rename(columns=column_mapping)
+    return dataframe
+
+#removing column
+# remove the "Max Poz" column from the DataFrame
+official_chart = official_chart.drop(columns=['Max Poz'])
+
+# which year is most frequent?
+year_counts = official_chart['Year'].value_counts()
+
+most_common_year = year_counts.index[0]
+
+print("The year that appears most frequently on the list is:", most_common_year)
+
+#count albums between 1960 and 1990
+
+# select the rows with years between 1960 and 1990 (inclusive)
+year_range = official_chart.loc[(official_chart['Year'] >= 1960) & (official_chart['Year'] <= 1990)]
+num_positions = len(year_range)
+print("The number of positions between 1960 and 1990 is:", num_positions)

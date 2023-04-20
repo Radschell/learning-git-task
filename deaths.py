6ff96f9f74ad4acf5bbf08ebdf8fde700feaa62a
radek.scheller <radoslaw.scheller@gmail.com>
2023-04-20 22:03:23 +0200

    counting number of deaths per race and the highest count with mental illness

--- deaths.py
+++ deaths.py
@@ -49,7 +49,7 @@ df = pd.read_csv(csv_path, dtype=data)
 # display the DataFrame
 print(df.head())
 
-# Adding function to count number of incidents per race
+# TASK 2. Adding function to count number of incidents per race
 df = pd.read_csv(csv_path, dtype=data)
 
 # group the records by race and count the number of records per race
@@ -57,3 +57,19 @@ race_counts = df.groupby('race').size().reset_index(name='counts')
 
 # display the race counts DataFrame
 print(race_counts)
+
+# group the records by race and mental illness status and count the number of records in each group
+race_and_mental_illness_counts = df.groupby(['race', 'signs_of_mental_illness']).size().reset_index(name='counts')
+
+# display the race and mental illness counts DataFrame
+print(race_and_mental_illness_counts)
+
+# TASK 3. Adding function to count which race has highest number of mental illnes incidents 
+# filter the DataFrame to include only records where signs_of_mental_illness is True
+mental_illness_records = race_and_mental_illness_counts[race_and_mental_illness_counts['signs_of_mental_illness'] == True]
+
+# get the race with the highest number of records where signs_of_mental_illness is True
+race_with_most_mental_illness_records = mental_illness_records.loc[mental_illness_records['counts'].idxmax()]['race']
+
+# print the race with the highest number of records where signs_of_mental_illness is True
+print("The race with the highest number of records where signs_of_mental_illness is True is:", race_with_most_mental_illness_records)
